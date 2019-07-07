a96616bb381a077267f601fb8e6d7647a6d2b83b
wojciech-florczak <florczak@wojciech.dev>
2019-07-07 16:56:45 +0200

    Feat: fix navigation settings, add placeholder background for lead component

--- src/components/Lead.js
+++ src/components/Lead.js
@@ -1,22 +1,28 @@
 import React from "react";
+
 import "./Lead.css";
+import placeholder from "../assets/images/placeholder-black.png";
 
 const Lead = ({movie}) => {
   // Set base url for image
   let baseImage = "https://image.tmdb.org/t/p/w1280";
+  let initialImage = placeholder;
   if(typeof movie.name !== "undefined") {
     // Change font size of title to smaller if title is too long
     if(movie.name.length >= 20 ) { document.querySelector('.lead__content__title').style.fontSize = "5em" }
   }
 
   return (
-    <div 
-      className="lead"
-      style={{ backgroundImage: `url(${baseImage}${movie.backdrop_path}` }}
-    >
-      {/* <div className="lead__background">
-        <img onError={e => e.target.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/A_blank_black_picture.jpg/1536px-A_blank_black_picture.jpg")} src={`${baseImage}${movie.backdrop_path}`} alt="background" className="lead__background__image"/>
-      </div> */}
+    <div className="lead">
+      <div className="lead__background">
+        <img
+          src={
+            typeof movie.backdrop_path == "undefined" ? initialImage : `${baseImage}${movie.backdrop_path}`
+          }
+          alt="background"
+          className="lead__background__image"
+        />
+      </div>
       <div className="overlay--black"></div>
       <div className="lead__content">
         <h2 className="lead__content__title">{movie.title || movie.name}</h2>
--- src/components/NavigationSettings.js
+++ src/components/NavigationSettings.js
@@ -6,27 +6,39 @@ import profile from "../assets/images/profile.png";
 const NavigationSettings = () => {
   return (
     <div className="navigation__profile">
-      <img
-        className="navigation__profile__avatar"
-        src={profile}
-        alt="profile"
-      />
-      <i className="navigation__profile__icon fas fa-caret-down" />
+      <div className="navigation__profile__account">
+        <img
+          className="navigation__profile__avatar"
+          src={profile}
+          alt="profile"
+        />
+        <i className="navigation__profile__icon fas fa-caret-down" />
+      </div>
       <div className="navigation__profile__dropdown">
-        <ul className="navigation__profile__dropdown__list">
-          <li className="navigation__profile__dropdown__item">
-            Manage Profiles
-          </li>
-          <li className="navigation__profile__dropdown__item">Kids</li>
-          <li className="navigation__profile__dropdown__item">Account</li>
-          <li className="navigation__profile__dropdown__item">Help Center</li>
-          <li className="navigation__profile__dropdown__item">
-            Sign out of Netflix
-          </li>
-        </ul>
+        <i className="navigation__profile__dropdown__icon fas fa-caret-up" />
+        <div className="navigation__profile__dropdown__content">
+          <ul className="navigation__profile__dropdown__list">
+            <li className="navigation__profile__dropdown__item">
+              <img className="navigation__profile__avatar" src={profile} alt="kids avatar"/>
+              Kids
+            </li>
+            <li className="navigation__profile__dropdown__item">
+              Manage Profiles
+            </li>
+          </ul>
+          <hr/>
+          <ul className="navigation__profile__dropdown__list">
+            <li className="navigation__profile__dropdown__item">Account</li>
+            <li className="navigation__profile__dropdown__item">Help Center</li>
+            <li className="navigation__profile__dropdown__item">
+              Sign out of Netflix
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 };
+
 
 export default NavigationSettings;
