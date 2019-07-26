52a6647e625b865a2d49480e552520e0202007c7
wojciech-florczak <florczak@wojciech.dev>
2019-07-26 22:27:45 +0200

    Feat: change logic in SearchTitle comp, add packet to deploy to gh pages

--- src/pages/SearchTitle.js
+++ src/pages/SearchTitle.js
@@ -6,30 +6,29 @@ import moviesdb from "../api/moviedb";
 
 const SearchTitle = ({ match }) => {
   const [title, setTitle] = useState([]);
-  const [type, setType] = useState("");
 
   let baseImage = "https://image.tmdb.org/t/p/w1280";
   let posterImage = "https://image.tmdb.org/t/p/w500";
-
-  // Make request for title details
-  const findTitle = async () => {
-    let res = await moviesdb.get(`${type}/${match.params.id}`, {
-      params: { api_key: process.env.REACT_APP_API }
-    });
-    setTitle(res.data);
-    console.log(res.data);
-  };
-  const isMovie = () => {
-    if (type === "") {
-      setType(match.path.includes("tv") ? "tv" : "movie");
-    }
-  };
-  isMovie();
-
+  
   useEffect(() => {
-    window.scrollTo(0, 0);
-    findTitle();
-  }, []);
+    const type = match.path.includes("tv") ? "tv" : "movie";
+    
+    const findTitle = async() => {
+      let res = await moviesdb.get(`${type}/${match.params.id}`, {
+        params: { api_key: process.env.REACT_APP_API }
+      });
+      
+      setTitle(res.data);
+      console.log(res.data)
+    }
+
+    if (title.length === 0)  {
+      window.scrollTo(0, 0);
+      findTitle();
+    }
+  });
+
+  const isTv = title.name;
 
   return (
     <div className="main-content">
@@ -43,17 +42,15 @@ const SearchTitle = ({ match }) => {
           </div>
           <div className="about__title__description">
             <h1>{title.title || title.name}</h1>
-            <h2>
-              {(title.original_title !== title.title
-                ? title.original_title
-                : null) ||
-                (title.original_name !== title.name
-                  ? title.original_name
-                  : null)}
-            </h2>
+            {(title.original_title !== title.title) || (title.original_name !== title.name) ? 
+              <h2>
+                {title.original_title || title.original_name}
+              </h2>
+            : null
+            } 
             <h2>{title.release_date || `${title.first_air_date} - ${title.last_air_date}`}</h2>
-            <h3>{title.vote_average}/10</h3>
-            <h3>{title.vote_count} votes</h3>
+            <h3>{`${title.vote_average}/10`}</h3>
+            <h3>{`${title.vote_count} votes`}</h3>
             {title.tagline ? <h2>{title.tagline}</h2> : null}
             {title.number_of_episodes ? (
               <h2>{`${title.number_of_seasons} season(s) ${
