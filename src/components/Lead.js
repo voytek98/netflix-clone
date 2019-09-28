dbcacc03f0c8230b214407a6674a643ea3e5ec03
wojciech-florczak <florczak@wojciech.dev>
2019-09-28 17:35:36 +0200

    Feat: add slideIn animation, add animations to lead component, add isLoading hook to lead component, fix background movie path, comment out function for checking title length

--- src/components/Lead.js
+++ src/components/Lead.js
@@ -1,41 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Lead.css";
 import placeholder from "../assets/images/placeholder-black.png";
 
 const Lead = ({movie}) => {
+  const [isLoading, setIsLoading] = useState(true);
   // Set base url for image
   let baseImage = "https://image.tmdb.org/t/p/w1280";
-  let initialImage = placeholder;
-  if(typeof movie.name !== "undefined") {
-    // Change font size of title to smaller if title is too long
-    if(movie.name.length >= 20 ) { document.querySelector('.lead__content__title').style.fontSize = "5em" }
-  }
+  // Change font size of title to smaller if title is too long
+    // if((movie.name.length || movie.title.length) >= 20 ) { document.querySelector('.lead__content__title').style.fontSize = "5em" }
+  useEffect(()=> {
+    if(movie.id) {
+      setIsLoading(false)
+    }
+  }, [movie])
 
   return (
     <div className="lead">
-      <div className="lead__background">
-        <img
-          src={
-            typeof movie.backdrop_path == ("undefined" || "null") ? initialImage : `${baseImage}${movie.backdrop_path}`
-          }
-          alt="background"
-          className="lead__background__image"
-        />
-      </div>
-      <div className="overlay--black"></div>
-      <div className="lead__content">
-        <h2 className="lead__content__title">{movie.title || movie.name}</h2>
-        <div className="lead__content__icons">
-          <button className="lead__content__icons__button">
-            <i className="lead__content__icons__icon fas fa-play" />Play
-          </button>
-          <button className="lead__content__icons__button">
-            <i className="lead__content__icons__icon fas fa-plus" />My list
-          </button>
+      {isLoading?
+        (
+          <div className="spinner">
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+          </div>
+        ) :
+        (<div className="lead-wrapper">
+        <div className="lead__background">
+          <img
+            src={
+              (movie.backdrop_path === null) ? placeholder : `${baseImage}${movie.backdrop_path}`
+            }
+            alt="background"
+            className="lead__background__image"
+          />
         </div>
-        <p className="lead__content__description">{movie.overview}</p>
-      </div>
+        <div className="overlay--black"></div>
+        <div className="lead__content">
+          <h2 className="lead__content__title">{movie.title || movie.name}</h2>
+          <div className="lead__content__icons">
+            <button className="lead__content__icons__button">
+              <i className="lead__content__icons__icon fas fa-play" />Play
+            </button>
+            <button className="lead__content__icons__button">
+              <i className="lead__content__icons__icon fas fa-plus" />My list
+            </button>
+          </div>
+          <p className="lead__content__description">{movie.overview}</p>
+        </div>
+      </div>)
+      }
     </div>
   );
 }
